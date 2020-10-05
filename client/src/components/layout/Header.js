017b873825c52690d27839bdb6ca46acb2fea9f1
Michaeldremy <michaeldremy@gmail.com>
2020-10-04 21:49:06 -0700

    awda

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header id="header">
       <Link to="/">
-        <h1 className="title">Enter Site Name</h1>
+        <h1 className="title">Enter Site awdaName</h1>
       </Link>
       {/* if we want to add more navigation we would add it here */}
       <AuthOptions />
